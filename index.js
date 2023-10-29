a367877afda942bb51967289271b3e31c66556c0
Tanjid-showkot <tanjidsaykot611@gmail.com>
2023-10-29 12:20:41 +0600

    add vercel

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors())
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+
+
+
+app.use(cors(corsOptions))
 app.use(express.json())
 require('dotenv').config();
 const port = process.env.PORT || 5000;
